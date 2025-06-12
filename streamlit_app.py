347e777860195abae5299e88f3d1adf35aefdc4d
Mireya Garcia Serrano <112426428+mireyaserrano@users.noreply.github.com>
2025-06-12 21:30:10 +0000

    add data and intro

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,21 @@
 import streamlit as st
+import altair as alt
+import pandas as pd
+import numpy as np
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+st.markdown(
+    "<h1 style='text-align: center;'>Understanding Failure Patterns in Voice Assistants</h1>",
+    unsafe_allow_html=True
 )
+st.markdown("""
+When voice assistants fail, not everyone is affected equally.  
+    Explore how technical failures in voice assistants intersect with user identity and behavior.  
+Our dashboard reveals patterns in failure type, accent-related disparities, and how users adapt—or don’t—after being misunderstood.
+
+Use the interactive charts below to dig into:
+- Which failures are most common and how they vary across user demographics
+- How users repeat or adjust speech after specific kinds of breakdowns
+- What design implications emerge when certain groups bear more burden than others
+""")
+
+df = pd.read_csv('voice-assistant-failures.csv')
